5a211572fa99edd22c5fb620d27d5abd50019404
= <=>
2024-06-03 10:40:33 +0700

    Update ProjectSection heading position

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -38,7 +38,7 @@ const ProjectSection = () => {
 
                 className="flex z-[1] flex-col h-screen items-center justify-center">
                 <motion.h1
-                    className="text-4xl font-semibold text-transparent mt-[1000px] md:mt-[20px] bg-clip-text bg-gradient-to-r from-purple-500 to-orange-500 mb-10"
+                    className="text-4xl font-semibold text-transparent mt-[1070px] md:mt-[20px] bg-clip-text bg-gradient-to-r from-purple-500 to-orange-500 mb-10"
                     variants={headingVariants}
                 >
                     My Projects
