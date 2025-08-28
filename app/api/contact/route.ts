6e2b545763ad2bc1f32bf76850fe6bcf7b25986a
Your Name <you@example.com>
2025-08-28 15:07:22 +0700

    revert runtime to nodejs in contact route

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export const POST = async (req: Request) => {
   const transporter = nodemailer.createTransport({
